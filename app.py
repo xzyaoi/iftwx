d163a9b94eebcd6aff4fb161088e35704e939581
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2017-08-29 22:33:30 +0800

    fix encoding

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flask import Flask
 from flask import request 
 import json
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 app = Flask(__name__)
 
 # 动态路由
@@ -14,24 +18,20 @@ def gitlab_hook():
     if request.method == 'POST':
         print 'Got request:', request.data
         data = json.loads(request.data)
-
+        if(data['object_kind']=='push')
         # gitlab hook data format https://docs.gitlab.com/ce/user/project/integrations/webhooks.html
-        ref = data['ref'].split('/', 2)[-1]
+            ref = data['refs'].split('/', 2)[-1]
 
-        commits = data['commits']
-        if commits and 'merge' in commits[-1]['message'].lower():
-            return ''
+            commits = data['commits']
+            if commits and 'merge' in commits[-1]['message'].lower():
+                return ''
 
-        for commit in commits:
-            message = u'{name} push to repo {repo}/{ref} \n check: {gitlab_url} \n {message}'.format(
-                name=data['user_name'],
-                repo=data['repository']['name'],
-                ref=ref,
-                message=commit['message'],
-                gitlab_url=commit['url'])
-            print message
-
-
-        # gitlab does not need feedback
-        # but if we don't `return`, flask will raise 500
+            for commit in commits:
+                message = u'{name} push to repo {repo}/{ref} \n check: {gitlab_url} \n {message}'.format(
+                    name=data['user_name'],
+                    repo=data['repository']['name'],
+                    ref=ref,
+                    message=commit['message'],
+                    gitlab_url=commit['url'])
+                print message
     return ''
