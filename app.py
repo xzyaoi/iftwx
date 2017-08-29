af723f6efd186f2424b7aba7bfe0e723d4697997
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2017-08-29 18:17:21 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+from datetime import datetime
+
+from flask import Flask
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+# 动态路由
+@app.route('/gitlab', methods=['POST'])
+def gitlab_hook():
+    if request.method == 'POST':
+        print 'Got request:', request.data
+        data = json.loads(request.data)
+
+        # gitlab hook data format https://docs.gitlab.com/ce/user/project/integrations/webhooks.html
+        ref = data['ref'].split('/', 2)[-1]
+
+        commits = data['commits']
+        if commits and 'merge' in commits[-1]['message'].lower():
+            return ''
+
+        for commit in commits:
+            task_ids = filter_asana_task_id(commit['message'])
+
+            for task_id in task_ids:
+                message = u'{name} push to repo {repo}/{ref} \n check: {gitlab_url} \n {message}'.format(
+                    name=data['user_name'],
+                    repo=data['repository']['name'],
+                    ref=ref,
+                    message=commit['message'],
+                    gitlab_url=commit['url'])
+                print message
+
+
+        # gitlab does not need feedback
+        # but if we don't `return`, flask will raise 500
+    return ''
