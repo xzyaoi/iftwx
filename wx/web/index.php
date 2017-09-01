3913831f05843f654ed079be71eed2aae6ac18fc
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2017-09-02 01:06:25 +0800

    try to integrate with slim framework

--- wx/web/index.php
+++ wx/web/index.php
@@ -5,10 +5,8 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-
+$app->get('/hello', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello World");
     return $response;
 });
 $app->run();
