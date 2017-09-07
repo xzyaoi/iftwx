63baffdbdc83fca4f6c5cb2cfc11f4dc41dca05d
Xiaozhe Yaoi <xiaozhe.yaoi@gmail.com>
2017-09-07 20:55:30 +0800

    很气人啊这个SDK

--- wx/web/index.php
+++ wx/web/index.php
@@ -1,18 +1,38 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
+use \EasyWeChat\Foundation\Application;
+use \EasyWeChat\Message;
+use \Parse\ParseClient;
+use \Parse\ParseUser;
+use \Parse\ParseException;
 
-$app = new \Slim\App;
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello World");
-    return $response;
-});
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-  $name = $request->getAttribute('name');
-  $response->getBody()->write("Hello, $name");
+// Parse Configuration
+ParseClient::initialize( 'zhulijun-app-id', null, 'an82as3aHDa62IFaJw' );
+ParseClient::setServerURL('https://cloud.yice.org.cn','zhulijun');
 
-  return $response;
+// Wx App Options
+$options = [
+  'debug'  => true,
+  'app_id' => 'wx1e47d3e921e8c9f7',
+  'secret' => '675e32e48a0c59416768d8d5ab400cf5',
+  'token'  => 'hch0629',
+  'aes_key' => 'oIjCxTnCwXVCD4bgeceS4d6Y44YFg5exKj6ybGs8xmD', // 可选
+  'log' => [
+      'level' => 'debug',
+      'file'  => '/tmp/easywechat.log',
+  ],
+];
+
+$app = new Application($options);
+$server = $app->server;
+
+$server->setMessageHandler(function ($message) {
+  if($message->Content=='ni'){
+    return 'it is ni!'
+  }
+  return $message->Content;
 });
-$app->run();
+
+$response = $server->serve();
+$response->send(); 
