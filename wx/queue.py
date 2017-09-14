7e26ca6e30b204a3c13ac8ee13199c9904fea9ed
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2017-09-15 00:39:07 +0800

    trying to disable ensure_ascii

--- wx/queue.py
+++ wx/queue.py
@@ -9,8 +9,6 @@ celery_app = Celery('zhulijun',broker='amqp://guest@localhost:5672//')
 
 @celery_app.task
 def send_template_message(openId,templateId,data,url=None,miniProgram=None):
-    data=json.dumps(data)
-    print data
+    data=json.dumps(data,ensure_ascii=False,indent=2)
     r=client.message.send_template(openId,templateId,data,url,miniProgram)
-    print r
     return r
