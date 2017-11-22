49f151c46690975495ad76d55083744c9be9fe1a
Xiaozhe Yaoi <xiaozhe.yaoi@gmail.com>
2017-11-22 21:10:16 +0800

    add login page

--- dashboard/src/router/index.ts
+++ dashboard/src/router/index.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/pages/Login.vue'
+import Container from '@/pages/Container.vue'
+import MessageApp from '@/pages/MessageApp.vue'
 
 Vue.use(Router)
 
@@ -10,6 +13,23 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path:'/app',
+      name:'App',
+      component:Container,
+      children:[
+        {
+          path:'message',
+          name:'Message',
+          component:MessageApp
+        }
+      ]
     }
   ]
 })
