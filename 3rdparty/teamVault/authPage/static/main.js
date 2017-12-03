6d0e08de14a545dbf8c79ec98a91c70a13f323e7
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2017-12-04 01:53:58 +0800

    minor fix

--- 3rdparty/teamVault/authPage/static/main.js
+++ 3rdparty/teamVault/authPage/static/main.js
@@ -1,15 +1,31 @@
+/**
+ * 
+ * Global Variables
+ */
+var socket = null
+
+/**
+ * @param {string} endpoint
+ */
 function getRequestUrl(endpoint) {
     var base = 'https://cloud.yice.org.cn/zhulijun/classes/'
     return base + endpoint
 }
 
+/**
+ * 
+ * @param {*} name 
+ */
 function getUrlParam(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) return unescape(r[2]);
-    return null;
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)")
+    var r = window.location.search.substr(1).match(reg)
+    if (r != null) return unescape(r[2])
+    return null
 }
 
+/**
+ * 
+ */
 function getUserInfo() {
     var userid = getUrlParam('userid')
     $.ajax({
@@ -32,10 +48,33 @@ function getUserInfo() {
     })
 }
 
+/**
+ * 
+ */
 function initSocket() {
-    var socket = io.connect('http://localhost');
-    socket.on('news', function(data) {
-        console.log(data);
-        socket.emit('my other event', { my: 'data' });
-    });
+    if (socket === null) {
+        socket = io.connect('http://localhost:8080')
+        socket.on('connect', function(data) {
+            console.log(data)
+            socket.emit('message', { my: 'data' })
+        });
+    } else {
+        console.warn('socket has been initialized.')
+    }
+}
+
+function authPasskey() {
+    if (socket === null) {
+        console.error('socket has not been initialized.')
+        return;
+    }
+    socket.emit('auth_success', { data: '123' })
+}
+
+function denyPasskey() {
+    if (socket === null) {
+        console.error('socket has not been initialized.')
+        return;
+    }
+    socket.emit('auth_denied', { data: '123' })
 }
--- 3rdparty/teamVault/ts/src/server.ts
+++ 3rdparty/teamVault/ts/src/server.ts
@@ -17,43 +17,35 @@ export class Server {
         this.listen()
     }  
     private createApp(): void {
-        this.app = new Koa();
+        this.app = new Koa()
     }
     private config(): void {
-        this.port = process.env.PORT || Server.PORT;        
+        this.port = process.env.PORT || Server.PORT     
     }
     private createServer(): void {
-        this.server = http.createServer(this.app.callback());        
+        this.server = http.createServer(this.app.callback())
     }
     private sockets(): void {
-        this.io = socketIo(this.server);        
+        this.io = socketIo(this.server)
     }
     private listen():void {
         this.server.listen(this.port, () => {
-            console.log('Running Server on port %s', this.port);
+            console.log('Running Server on port %s', this.port)
         });
         this.io.on('connect', (socket: any) => {
-            console.log('Connected client on port %s.', this.port);
-            console.log('With socket:')
-            console.log(socket)
+            console.log('Connected client on port %s.', this.port)
+            console.log(socket.id)
             socket.on('message', (m: Message) => {
-                console.log('[Server](message): %s', JSON.stringify(m));
-                this.io.emit('message', m);
-            });
-            socket.on('requireAuth',(m, Message) => {
-                console.log('[Auth Required]')                
-                console.log('[Auth](message): %s', JSON.stringify(m));
-                
-            });
-            socket.on('authComplete',(m, Message) => {
-                console.log('[Auth Completed]')
-                console.log('[Auth](message): %s')
+                console.log('[Server](message): %s', JSON.stringify(m))
+                this.io.emit('message', m)
             })
-            socket.on('disconnect', () => {
-                console.log('Client Disconnected');
-            });
-        });
-        
-        this.io.on('')
+            socket.on('auth_success', (m: Message) => {
+                console.log('[Server](message): %s', JSON.stringify(m))
+                this.io.emit('message', m)
+            })
+            socket.on('auth_required',(m:Message) => {
+                
+            })
+        })
     }
 }
