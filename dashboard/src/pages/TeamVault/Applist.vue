5f6a00ba4e062a7804db63605c1fc9d5d10f8f4c
yaoxiaozhe@email.szu.edu.cn <yaoxiaozhe@email.szu.edu.cn>
2017-12-04 14:59:39 +0800

    add interface of adding vault

--- dashboard/src/pages/TeamVault/Applist.vue
+++ dashboard/src/pages/TeamVault/Applist.vue
@@ -1,17 +1,44 @@
 <template>
 <div class="container">
-      <v-breadcrumbs>
-      <v-icon slot="divider">chevron_right</v-icon>
+  <v-layout row justify-center>
+    <v-dialog v-model="is_create_dialog_open" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">创建保险柜</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-text-field label="保险柜名称" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-checkbox label="是否公开" v-model="is_public" light></v-checkbox>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <small>* 为必填项</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="is_create_dialog_open = false">放弃</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="is_create_dialog_open = false">完成</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+  <v-breadcrumbs>
+    <v-icon slot="divider">chevron_right</v-icon>
       <v-breadcrumbs-item>
         Team Vault
       </v-breadcrumbs-item>
       <v-breadcrumbs-item>
-        目录列表
+        保险柜列表
       </v-breadcrumbs-item>
     </v-breadcrumbs>
-    <v-card class="channel_container">
-    <v-card-title>
-      我的应用
+  <v-card class="channel_container">
+  <v-card-title>
+    我的保险柜
       <v-spacer></v-spacer>
         <v-select
             v-model="selected_channel"
@@ -21,7 +48,7 @@
             item-value="name"
             autocomplete
         ></v-select>
-        <v-btn color="green darken-1" flat="flat">创建目录</v-btn>
+        <v-btn color="blue darken-1" flat="flat" @click="createVault">创建保险柜</v-btn>
     </v-card-title>
     <v-data-table
         v-bind:headers="headers"
@@ -43,57 +70,59 @@
 </template>
 
 <script lang="ts">
-import store from '../../store'
-import router from '../../router'
-import { Channel } from '../../models/channel'
-import Vue from 'vue'
+import store from "../../store";
+import router from "../../router";
+import { Channel } from "../../models/channel";
+import Vue from "vue";
 export default Vue.extend({
-  data: ()=>({
+  data: () => ({
     listedChannel: [],
-    select_items:[],
-    selected_channel: '',
-    max25chars: (v:string) => v.length <= 25 || 'Input too long!',
-    search: '',
-    tmp: '',
+    select_items: [],
+    selected_channel: "",
+    is_create_dialog_open : false,
+    is_public: false,
+    max25chars: (v: string) => v.length <= 25 || "Input too long!",
+    search: "",
+    tmp: "",
     pagination: {},
     headers: [
-          {
-            text: '频道ID',
-            align: 'left',
-            sortable: false,
-            value: 'objectId'
-          },
-          { text: '频道名', value: 'name', align:'left' },
-          { text: '创建时间', value: 'createdAt', align:'left' },
-          { text: '关注人数', value: 'follower', align:'left' }
-        ],
-
+      {
+        text: "频道ID",
+        align: "left",
+        sortable: false,
+        value: "objectId"
+      },
+      { text: "频道名", value: "name", align: "left" },
+      { text: "创建时间", value: "createdAt", align: "left" },
+      { text: "关注人数", value: "follower", align: "left" }
+    ]
   }),
   methods: {
+    createVault() {
+      this.is_create_dialog_open = true
+    },
     getChannels() {
-      let self = this
-      store.dispatch('getChannels').then(function(res){
-        self.listedChannel = res.map(function(each:Channel){
-          return each.toJSON()
-        })
-        self.select_items = res.map(function(each:Channel){
-          return each.toJSON().name
-        })
-        console.log(self.listedChannel)
-      })
+      let self = this;
+      store.dispatch("getChannels").then(function(res) {
+        self.listedChannel = res.map(function(each: Channel) {
+          return each.toJSON();
+        });
+        self.select_items = res.map(function(each: Channel) {
+          return each.toJSON().name;
+        });
+        console.log(self.listedChannel);
+      });
     }
   },
-  created () {
-    this.getChannels()
-  },
-
-})
-
+  created() {
+    this.getChannels();
+  }
+});
 </script>
 
 <style scopedSlots>
 .channel_container {
-  width:100%;
-  margin-top:0px;
+  width: 100%;
+  margin-top: 0px;
 }
 </style>
